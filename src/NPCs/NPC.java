144dcaa7c32e2af91fd5536576709ca9bc14e893
BvDijkCalco <47522232+BvDijkCalco@users.noreply.github.com>
2019-02-21 09:48:13 +0100

    Added almost no shit at all for the implementations of npc

---
+++ src/NPCs/NPC.java
@@ -0,0 +1,8 @@
+package NPCs;
+
+public class NPC {
+    String name;
+    String id = "NPC";
+    String desc;
+    int hp = 100;
+}
---
+++ src/NPCs/Skeleton.java
@@ -0,0 +1,10 @@
+package NPCs;
+
+public class Skeleton extends NPC {
+    public Skeleton() {
+        this.name = "Skeleton";
+        this.id = "genericSkeleton";
+        this.desc = "A creature of death.";
+        this.hp = 50;
+    }
+}
